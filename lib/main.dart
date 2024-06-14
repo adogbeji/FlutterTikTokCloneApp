80267f663274d7357e7a1bd518eb45773ed350e8
adogbeji <baka_1993@yahoo.co.uk>
2024-06-14 11:28:14 +0100

    Change MaterialApp --> GetMaterialApp in main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:get/get.dart';
 import 'package:tiktok_clone/auth/login_screen.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'TikTok Clone',
       theme: ThemeData.dark().copyWith(
