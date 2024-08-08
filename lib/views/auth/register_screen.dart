cc2d0324f323a567a60057eb073aeed66e674c71
adogbeji <baka_1993@yahoo.co.uk>
2024-08-08 11:14:34 +0100

    Start building register screen

--- lib/views/auth/register_screen.dart
+++ lib/views/auth/register_screen.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+import 'package:simple_circular_progress_bar/simple_circular_progress_bar.dart';
+import 'package:get/get.dart';
+
+import 'package:tiktok_clone/views/auth/register_screen.dart';
+import 'package:tiktok_clone/views/widgets/input_text_widget.dart';
+
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
@@ -8,11 +15,142 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  // Username, Email & Password Controllers
+  TextEditingController userNameTextEditingController = TextEditingController();
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+  bool showProgressBar = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Register Screen'),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 100,),
+
+              Image.asset(
+                'assets/images/tiktok.png',
+                width: 200,
+              ),
+
+              Text(
+                'Welcome',
+                style: GoogleFonts.acme(
+                  fontSize: 34,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              Text(
+                'Glad to see you!',
+                style: GoogleFonts.acme(
+                  fontSize: 34,
+                  color: Colors.grey,
+                ),
+              ),
+
+              const SizedBox(height: 30,),
+
+              // EMAIL INPUT FIELD
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: InputTextWidget(
+                  textEditingController: emailTextEditingController,
+                  labelString: 'Email',
+                  iconData: Icons.email_outlined,
+                  isObscure: false,
+                ),
+              ),
+
+              const SizedBox(height: 15,),
+
+              // PASWORD INPUT FIELD
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: InputTextWidget(
+                  textEditingController: passwordTextEditingController,
+                  labelString: 'Password',
+                  iconData: Icons.lock_outline,
+                  isObscure: true,
+                ),
+              ),
+
+              SizedBox(height: 12,),
+
+              // LOGIN BUTTON
+              showProgressBar == false ?
+              Column(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width - 38,
+                    height: 54,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          showProgressBar = true;
+                        });
+
+                        // Log in user
+                      },
+                      child: const Center(
+                        child: Text('Log In', style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w700,
+                        ),),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 7,),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Don\'t have an account?', style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey,
+                      ),),
+
+                      InkWell(
+                        onTap: () {
+                          // Sends user to register screen
+                          Get.to(const RegisterScreen());
+                        },
+                        child: const Text('Sign up now', style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),),
+                      ),
+                    ],
+                  ),
+                ],
+              ): Container(
+                child: const SimpleCircularProgressBar(
+                  progressColors: [
+                    Colors.green,
+                    Colors.blueAccent,
+                    Colors.red,
+                    Colors.amber,
+                    Colors.purpleAccent,
+                  ],
+                  animationDuration: 3,
+                  backColor: Colors.white38,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
