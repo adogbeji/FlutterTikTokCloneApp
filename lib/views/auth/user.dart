4039949a15137ce84a9a41f760becc4178526860
adogbeji <baka_1993@yahoo.co.uk>
2024-08-09 19:39:50 +0100

    Start creating User model class

---
+++ lib/views/auth/user.dart
@@ -0,0 +1,21 @@
+class User {
+  String? name;
+  String? uid;
+  String? image;
+  String? email;
+  String? youtube;
+  String? facebook;
+  String? twitter;
+  String? instagram;
+
+  User({
+    this.name,
+    this.uid,
+    this.image,
+    this.email,
+    this.youtube,
+    this.facebook,
+    this.twitter,
+    this.instagram,
+  });
+}
