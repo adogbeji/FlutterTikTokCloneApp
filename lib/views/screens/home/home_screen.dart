30274bef9b584f04649b379064b721deea0da4c1
adogbeji <baka_1993@yahoo.co.uk>
2024-08-28 11:14:34 +0100

    Add variable to home screen

--- lib/views/screens/home/home_screen.dart
+++ lib/views/screens/home/home_screen.dart
@@ -8,6 +8,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int screenIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
