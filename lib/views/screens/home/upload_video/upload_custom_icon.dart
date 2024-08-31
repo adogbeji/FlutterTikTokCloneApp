41c713ed7d0cc6e311ad0abbdc5e8b84907e77bb
adogbeji <baka_1993@yahoo.co.uk>
2024-08-31 11:17:09 +0100

    Start building UploadCustomIcon widget

--- lib/views/screens/home/upload_video/upload_custom_icon.dart
+++ lib/views/screens/home/upload_video/upload_custom_icon.dart
@@ -5,9 +5,20 @@ class UploadCustomIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Upload Custom Icon'),
+    return SizedBox(
+      width: 46,
+      height: 32,
+      child: Stack(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left: 12),
+            width: 40,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(256, 250, 46, 108),
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ],
       ),
     );
   }
