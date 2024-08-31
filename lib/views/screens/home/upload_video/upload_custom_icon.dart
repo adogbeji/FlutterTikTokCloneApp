74d1562380cfa0185948b3d4d5a91be77efe7704
adogbeji <baka_1993@yahoo.co.uk>
2024-08-31 18:42:37 +0100

    Add icon to UploadCustomIcon widget

--- lib/views/screens/home/upload_video/upload_custom_icon.dart
+++ lib/views/screens/home/upload_video/upload_custom_icon.dart
@@ -18,6 +18,31 @@ class UploadCustomIcon extends StatelessWidget {
               borderRadius: BorderRadius.circular(8),
             ),
           ),
+
+          Container(
+            margin: const EdgeInsets.only(right: 12),
+            width: 40,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(256, 32, 212, 235),
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+
+          Center(
+            child: Container(
+              height: double.infinity,
+              width: 40,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Icon(
+                Icons.add,
+                color: Colors.black,
+                size: 24,
+              ),
+            ),
+          ),
         ],
       ),
     );
