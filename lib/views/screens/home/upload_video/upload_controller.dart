a15d7ef312955e1a6019f687a06e73046895a262
adogbeji <baka_1993@yahoo.co.uk>
2024-10-04 20:05:41 +0100

    Start using video_compress in UploadController

--- lib/views/screens/home/upload_video/upload_controller.dart
+++ lib/views/screens/home/upload_video/upload_controller.dart
@@ -1,6 +1,9 @@
 import 'package:get/get.dart';
+import 'package:video_compress/video_compress.dart';
 
 class UploadController extends GetxController {
 
-  compressVideoFile(String videoFilePath) {}
+  compressVideoFile(String videoFilePath) async {
+    await VideoCompress.compressVideo(path)
+  }
 }
