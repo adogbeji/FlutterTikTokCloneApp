d0e0e005d3573a43fc10d15595fd3c340aee7750
adogbeji <baka_1993@yahoo.co.uk>
2024-11-05 11:14:32 +0000

    Add more variables to video model class

--- lib/views/screens/home/upload_video/video.dart
+++ lib/views/screens/home/upload_video/video.dart
@@ -1,4 +1,12 @@
 class Video {
   String? userID;
   String? userName;
+  String? videoID;
+  int? totalComments;
+  List? likesList;
+  String? artistSongName;
+  String? descriptionTags;
+  String? videoUrl;
+  String? thumbnailUrl;
+  int? publishedDateTime;
 }
